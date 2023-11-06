ec70940cf59ce05724a08281cbf8126c22a264d3
unknown <marjastrow@gmail.com>
2023-11-06 13:57:26 -0600

    Completed Project 1

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// welcome message
+var user = 'jill';
+var salutation = 'Hello, ';
+var greeting = salutation + user + '! Here are the latest thoughts on gun violence.';
+var greetingEl = document.getElementById('greeting');
+
+greeting.textContent = greeting;
